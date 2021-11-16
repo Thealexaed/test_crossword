182a550759cdce7456655e2ee665594979a1943e
Thealexaed <83904171+Thealexaed@users.noreply.github.com>
2021-11-16 10:18:01 +0300

    Update get_crossword.py

--- get_crossword.py
+++ get_crossword.py
@@ -1,1 +1,49 @@
+# Функция получения входных данных и построения кроссворда
+import sys
+from test_crossword.functions.get_words import find_words
+from test_crossword.functions.get_definitions import definition_search
+from test_crossword.functions.crossword_plotter import print_words
 
+def get_crossword(request_word, count_of_words, print_answers):
+  try:
+    if request_word == '':
+      print('Введите слово!')
+    else:
+      try:
+        final_def(request_word, count_of_words, print_answers)
+      except:
+        sys.stdout.write('\rСлова не найдены! Уточните запрос!')
+  except KeyboardInterrupt:
+    sys.stdout.write('\rПроцесс остановлен!')
+    pass
+
+
+# Функция составления кроссворда
+def final_def(word, count_of_words, print_answers):
+    text = '\rИщу слова...'
+    sys.stdout.write(text)
+    if ',' in word:
+      words = word.split(',')
+      words = [word.strip() for word in words]
+      request_word = 'Своя тема'
+    else:
+      words = find_words(word, count_of_words)
+      request_word = word
+    words = [i.lower() for i in words]
+    if word.lower() not in words and ' ' not in word:
+      words.append(word.lower())
+    
+    text = '\rИщу определения к словам...'
+    sys.stdout.write(text)
+    definitions = definition_search(words)
+    words = list(definitions.keys())
+
+    text = '\rИдет построение сканворда...'
+    sys.stdout.write(text)
+
+    print_words(words,
+                n_words = count_of_words,
+                random_sort=True,
+                answers=print_answers,
+                definitions=definitions,
+                request_word = request_word)
