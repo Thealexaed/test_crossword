6c36a4ba362e4d20cdc63f87daed28e8f39bd4bb
Thealexaed <83904171+Thealexaed@users.noreply.github.com>
2021-12-11 13:01:15 +0300

    Update get_crossword.py

--- get_crossword.py
+++ get_crossword.py
@@ -5,7 +5,7 @@ from wiki_crossword.functions.get_words import find_words
 from wiki_crossword.functions.get_definitions import definition_search
 from wiki_crossword.functions.crossword_plotter import print_words
 
-def get_crossword(request_word, count_of_words=5, print_answers=False, difficult='Очень сложно'):
+def get_crossword(request_word, count_of_words=5, print_answers=False, difficult='Сложно'):
   difficult_dict = {
     'Легко':1,
     'Средне':2,
@@ -26,7 +26,6 @@ def get_crossword(request_word, count_of_words=5, print_answers=False, difficult
     pass
 
 
-# Функция составления кроссворда
 def final_def(word, count_of_words, print_answers, difficult):
     text = '\rИщу слова...'
     sys.stdout.write(text)
@@ -34,9 +33,11 @@ def final_def(word, count_of_words, print_answers, difficult):
       words = word.split(',')
       words = [word.strip() for word in words]
       request_word = 'Своя тема'
+      dict_titles = dict()
     else:
       words, dict_titles = find_words(word, count_of_words)
       request_word = word
+
     words = [i.lower() for i in words]
 
     if word.lower() not in words and ' ' not in word:
